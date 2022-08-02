49921ca10171b4b870006d6d5b1d41ad599e9610
Gene Culling <culling66@gmail.com>
2022-08-02 22:37:15 +1200

    Load and Save from Servlet using ActiveObjects

--- src/main/java/com/geneculling/javakata/gifrepo/GifServlet.java
+++ src/main/java/com/geneculling/javakata/gifrepo/GifServlet.java
@@ -2,6 +2,7 @@ package com.geneculling.javakata.gifrepo;
 
 import com.atlassian.activeobjects.external.ActiveObjects;
 import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+import com.atlassian.sal.api.transaction.TransactionCallback;
 
 import javax.inject.Inject;
 import javax.servlet.ServletException;
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import static com.google.common.base.Preconditions.*;
 
@@ -24,11 +26,42 @@ public class GifServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.sendError(501, "not implemented");
+        final PrintWriter writer = response.getWriter();
+        writer.write("<h1>Gifs</h1>");
+
+        ao.executeInTransaction(new TransactionCallback<Void>()
+        {
+            @Override
+            public Void doInTransaction()
+            {
+                for (Gif gif : ao.find(Gif.class))
+                {
+                    writer.printf("<p>%s <img src=\"%2$s\"/></p>", gif.getName(), gif.getUrl());
+                }
+                return null;
+            }
+        });
+        writer.close();
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.sendError(501, "not implemented");
+        final String name = request.getParameter("name");
+        final String url = request.getParameter("url");
+
+        ao.executeInTransaction(
+                new TransactionCallback<Gif>(){
+            @Override
+            public Gif doInTransaction()
+            {
+                final Gif gif = ao.create(Gif.class);
+                gif.setName(name);
+                gif.setUrl(url);
+                gif.save();
+                return gif;
+            }
+        });
+
+        response.sendRedirect(request.getContextPath() + "/plugins/servlet/gifrepo");
     }
 }
